b3ef0e3b027fd86770a619b85f23728f62542fd6
PixelRifts <njk777400@gmail.com>
2021-03-11 19:19:35 +0530

    Make Azurite Layer for KeyCodes, MouseButtons and GamepadButtons

---
+++ src/main/java/input/GamepadButtons.java
@@ -0,0 +1,26 @@
+package input;
+
+public class GamepadButtons {
+	/** Gamepad buttons. See <a target="_blank" href="http://www.glfw.org/docs/latest/input.html#gamepad">gamepad</a> for how these are used. */
+	public static final int
+			AZ_BUTTON_A            = 0,
+			AZ_BUTTON_B            = 1,
+			AZ_BUTTON_X            = 2,
+			AZ_BUTTON_Y            = 3,
+			AZ_BUTTON_LEFT_BUMPER  = 4,
+			AZ_BUTTON_RIGHT_BUMPER = 5,
+			AZ_BUTTON_BACK         = 6,
+			AZ_BUTTON_START        = 7,
+			AZ_BUTTON_GUIDE        = 8,
+			AZ_BUTTON_LEFT_THUMB   = 9,
+			AZ_BUTTON_RIGHT_THUMB  = 10,
+			AZ_BUTTON_DPAD_UP      = 11,
+			AZ_BUTTON_DPAD_RIGHT   = 12,
+			AZ_BUTTON_DPAD_DOWN    = 13,
+			AZ_BUTTON_DPAD_LEFT    = 14,
+			AZ_BUTTON_LAST         = AZ_BUTTON_DPAD_LEFT,
+			AZ_BUTTON_CROSS        = AZ_BUTTON_A,
+			AZ_BUTTON_CIRCLE       = AZ_BUTTON_B,
+			AZ_BUTTON_SQUARE       = AZ_BUTTON_X,
+			AZ_BUTTON_TRIANGLE     = AZ_BUTTON_Y;
+}
