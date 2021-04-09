6659d03ef8ed73c0d0f5692adf499928eec6f928
PixelRifts <njk777400@gmail.com>
2021-04-09 21:34:41 +0530

    Add Javadocs for Debug Stuff

--- src/main/java/util/debug/DebugPrimitive.java
+++ src/main/java/util/debug/DebugPrimitive.java
@@ -1,12 +1,22 @@
 package util.debug;
 
+/**
+ * A Primitive that consists of a bunch of lines.
+ * These lines are rendered by the DebugRenderer
+ */
 public class DebugPrimitive {
+	/** The lines making up this primitive */
 	protected DebugLine[] lines;
 
+	/**
+	 * Create a primitive with the given lines
+	 * @param lines the lines making up the primitive
+	 */
 	public DebugPrimitive(DebugLine[] lines) {
 		this.lines = lines;
 	}
 
+	/** Get the lines making up the primitive */
 	public DebugLine[] getLines() {
 		return lines;
 	}
