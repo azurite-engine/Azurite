5bb3161ec029d5b9907a9c32f0c2d3f8cb788d48
PixelRifts <njk777400@gmail.com>
2021-04-10 13:54:10 +0530

    Add Javadocs to Animation, Mouse and Sprite

--- src/main/java/ecs/Sprite.java
+++ src/main/java/ecs/Sprite.java
@@ -8,12 +8,13 @@ import org.joml.Vector2f;
  */
 
 public class Sprite {
-
+	/** The texture for this sprite */
 	private Texture texture;
 
+	/** This sprite's texture coords */
 	private Vector2f[] textureCoordinates;
 	
-	// This contains the default coordinates for an entire image UV
+	/** This contains the default coordinates for an entire image UV */
 	private Vector2f[] defaultTextureCoordinates = {
 		new Vector2f(0, 0),
 		new Vector2f(0, 1),
@@ -23,8 +24,8 @@ public class Sprite {
 
 	/**
 	 * Construct a Sprite using custom texture coordinates (uv).
-	 * @param texture
-	 * @param uv
+	 * @param texture The texture for this sprite
+	 * @param uv The sprite's texture coords
 	 */
 	public Sprite (Texture texture, Vector2f[] uv) {
 		this.texture = texture;
@@ -33,33 +34,39 @@ public class Sprite {
 
 	/**
 	 * Construct a texture using default texture coordinates.
-	 * @param texture
+	 * @param texture The texture for this sprite
 	 */
 	public Sprite (Texture texture) {
 		this.texture = texture;
 		this.textureCoordinates = defaultTextureCoordinates;
 	}
 
+	/** Get this sprite's texture */
 	public Texture getTexture () {
 		return this.texture;
 	}
-	
+
+	/** Get this sprite's texture coordinates*/
 	public Vector2f[] getTextureCoordinates () {
 		return this.textureCoordinates;
 	}
 
+	/** Get the texture id of this sprite */
 	public int getTextureID () {
 		return texture.getTextureID();
 	}
 
+	/** Get the width of the texture */
 	public float getWidth () {
 		return texture.getWidth();
 	}
 
+	/** Get the height of the texture */
 	public float getHeight () {
 		return texture.getHeight();
 	}
 
+	/** Set this sprite's texture */
 	public void setTexture(Texture texture) {
 		this.texture = texture;
 	}
--- src/main/java/input/Mouse.java
+++ src/main/java/input/Mouse.java
@@ -11,22 +11,35 @@ import java.nio.DoubleBuffer;
 import static org.lwjgl.glfw.GLFW.*;
 
 public class Mouse {
-
+	/** Position of the mouse */
 	public static Vector2f mouse;
+	/** X Position of the mouse */
 	public static long mouseX = 0;
+	/** Y Position of the mouse */
 	public static long mouseY = 0;
+	/** Position of the mouse on the previous frame */
 	public static Vector2f pmouse;
+	/** X Position of the mouse on the previous frame */
 	public static long pmouseX = 0;
+	/** Y Position of the mouse on the previous frame */
 	public static long pmouseY = 0;
+	/** Mouse Scrolling Values */
 	public static Vector2f mouseScroll;
+	/** Mouse Scrolling in X-axis. */
 	public static float scrollX = 0;
+	/** Mouse Scrolling in Y-axis. */
 	public static float scrollY = 0;
+	/** Array for storing states for mouse buttons */
 	public static boolean mouseButton[] = new boolean[3];
+	/** Is the mouse being dragged */
 	public static boolean mouseDragged;
 
+	/** Latest button whose state was changed */
 	private static int _button;
+	/** Latest button whose state is changed */
 	private static int _action;
 
+	/** Apply the latest changes to the mouseButton array */
 	public static void pollMouseButtons() {
 		if (_action == GLFW_PRESS) {
 			if (_button < mouseButton.length)
@@ -58,6 +71,9 @@ public class Mouse {
 		});
 	}
 
+	/**
+	 * Called each frame to update mouse button states and mouse position.
+	 */
 	public static void update() {
 		pollMouseButtons();
 		
@@ -81,13 +97,21 @@ public class Mouse {
 		}
 	}
 
+	/**
+	 * Get if the queried button is pressed
+	 * @param button The button. Check input/Buttons.java
+	 * @return Returns whether the button is currently pressed.
+	 */
 	public static boolean mouseButtonDown (int button) {
 		if (button < mouseButton.length) {
 			return mouseButton[button];
 		}
 		return false;
 	}
-	
+
+	/**
+	 * Clear the mouse state
+	 */
 	public static void clearMouseInput () {
 		scrollX = 0;
 		scrollY = 0;
