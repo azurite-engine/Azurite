0f20f10b7aa9073387731cd0e3fc084507e89228
The-Aemogie <theaemogie@gmail.com>
2021-07-08 22:06:17 +0530

    :bulb: Added JavaDocs to Engine and Window classes. WIP

--- src/main/java/graphics/Window.java
+++ src/main/java/graphics/Window.java
@@ -15,156 +15,239 @@ import util.Engine;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 
+/**
+ * The {@code Window} handles scenes, and runs the main loop.
+ */
 public class Window {
-
+    
+    /**
+     * SceneManager. Need a better explanation? The thingy that does things with scenes.
+     */
     private SceneManager sceneManager;
-
+    
     // Window Variables
+    /**
+     * PLEASE GET RID OF THIS AND USE DELTA TIME VAR INSTEAD! I HATE THIS SYSTEM!
+     */
     private long frameCount = 0;
-
+    
+    /**
+     * The title of the window.
+     */
     private String title;
+    /**
+     * The memory address of the {@code GLFWwindow}
+     */
     private static long glfwWindow;
+    /**
+     * Honestly, even I don't know...
+     */
     private final GLFWVidMode videoMode;
-
-    private static int width;
-    private static int height;
+    /**
+     * The width and height of the window.
+     */
+    private static int width, height;
+    /**
+     * Whether to recalculate the camera's projection matrix when the window is resized.
+     */
     private final boolean recalculateProjectionOnResize;
-
+    
+    /**
+     * Window constructor.
+     * @param pwidth Width of the window.
+     * @param pheight Height of the window.
+     * @param ptitle Initial title of the window.
+     * @param fullscreen Whether the window should be fullscreen.
+     * @param minSceneLighting Minimum light level of the scene. (0.0F - 1.0F)
+     * @param recalculateProjectionOnResize Whether to recalculate the camera's projection matrix when the window is resized.
+     */
     public Window(int pwidth, int pheight, String ptitle, boolean fullscreen, float minSceneLighting, boolean recalculateProjectionOnResize) {
         videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         width = pwidth;
         height = pheight;
         title = ptitle;
         this.recalculateProjectionOnResize = recalculateProjectionOnResize;
-
+        
         //create the sceneManager to be able to set a scene
         sceneManager = new SceneManager();
-
+        
         sceneManager.setMinSceneLight(minSceneLighting);
-
+        
         // Configure GLFW
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
+        
         if (!fullscreen)
             initWindow(width, height, title, 0);
         else
             initWindow(width, height, title, glfwGetPrimaryMonitor());
     }
-
+    
+    /**
+     * Window constructor. The width and height are taken from {@link Window#videoMode}.
+     * @param ptitle Initial title of the window.
+     * @param minSceneLighting Minimum light level of the scene. (0.0F - 1.0F)
+     * @param recalculateProjectionOnResize Whether to recalculate the camera's projection matrix when the window is resized.
+     */
     public Window(String ptitle, float minSceneLighting, boolean recalculateProjectionOnResize) {
         videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         width = videoMode.width();
         height = videoMode.height();
         title = ptitle;
         this.recalculateProjectionOnResize = recalculateProjectionOnResize;
-
+        
         //create the sceneManager to be able to set a scene
         sceneManager = new SceneManager();
-
+        
         sceneManager.setMinSceneLight(minSceneLighting);
-
+        
         // Configure GLFW
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
+        
         initWindow(width, height, title, glfwGetPrimaryMonitor());
     }
-
+    
+    /**
+     * Overload constructor with {@code fullscreen} set to {@code false}.
+     * @see Window#Window(int, int, String, boolean, float, boolean)
+     */
     public Window(int pwidth, int pheight, String ptitle, float minSceneLighting, boolean recalculateProjectionOnResize) {
         this(pwidth, pheight, ptitle, false, minSceneLighting, recalculateProjectionOnResize);
     }
-
+    
+    /**
+     * Overload constructor with {@code minSceneLighting} set to {@code 1.0F}.
+     * @see Window#Window(int, int, String, float, boolean)
+     */
     public Window(int pwidth, int pheight, String ptitle, boolean recalculateProjectionOnResize) {
         this(pwidth, pheight, ptitle, 1.0f, recalculateProjectionOnResize);
     }
-
+    
+    /**
+     * Overload constructor with {@code minSceneLighting} set to {@code 1.0F}.
+     * @see Window#Window(String, float, boolean)
+     */
     public Window(String ptitle, boolean recalculateProjectionOnResize) {
         this(ptitle, 1.0f, recalculateProjectionOnResize);
     }
-
+    
+    /**
+     * Overload constructor with {@code recalculateProjectionOnResize} set to {@code false}.
+     * @see Window#Window(String, boolean)
+     */
     public Window(String ptitle) {
         this(ptitle, false);
     }
-
+    
+    /**
+     * Initialize the window.
+     * <li>Create {@code GLFWwindow}.</li>
+     * <li>Set up callbacks.</li>
+     * <li>Make the window context current, so that OpenGL can recognize it.</li>
+     * <li>Set the window position.</li>
+     * <li>I don't understand the next line.</li>
+     * @param width Width of the {@code GLFWwindow} to be created.
+     * @param height Height of the {@code GLFWwindow} to be created.
+     * @param title Title of the {@code GLFWwindow} to be created.
+     * @param monitor Another thing I don't understand.
+     */
     private void initWindow(int width, int height, String title, long monitor) {
         // Create window
         glfwWindow = glfwCreateWindow(width, height, title, monitor, 0);
-
+        
         if (glfwWindow == 0)
             throw new IllegalStateException("[FATAL] Failed to create window.");
-
+        
         // Set up callback
         glfwSetWindowSizeCallback(glfwWindow, (w, newWidth, newHeight) -> {
             Window.setWidth(newWidth);
             Window.setHeight(newHeight);
-
+            
             if (recalculateProjectionOnResize && currentScene().camera() != null)
                 currentScene().camera().adjustProjection();
             Events.windowResizeEvent.onEvent(new EventData.WindowResizeEventData(newWidth, newHeight));
         });
-
+        
         Mouse.setupCallbacks();
         Keyboard.setupCallbacks();
-
+        
         // Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
-
+        
         // Enable V-Sync
 //        glfwSwapInterval(1);
-
+        
         // Center the window
         glfwSetWindowPos(glfwWindow, (videoMode.width() - width) / 2, (videoMode.height() - height) / 2);
         GL.createCapabilities();
-
+        
     }
-
+    
+    /**
+     * Oh god not this...
+     */
     void getFPS() {
         //TODO this wont properly display the FPS it will just count up the frames, there is no reset after a second yet
         frameCount++;
         glfwSetWindowTitle(glfwWindow, title + " @ " + Math.round((frameCount / (Engine.millisRunning() / 1000))) + " FPS");
     }
-
+    
+    /**
+     * @return The memory address of the {@code GLFWwindow}.
+     */
     public static long glfwWindow() {
         return glfwWindow;
     }
-
+    
+    /**
+     * @return The window's title.
+     */
     public String getTitle() {
         return title;
     }
-
+    
+    /**
+     * @param title New title.
+     */
     public void setTitle(String title) {
         this.title = title;
         glfwSetWindowTitle(glfwWindow, title);
     }
-
+    
+    //Avoiding use of "game" loop as this could be used for non game stuff too, potentially.
+    /**
+     * Make the window visible.<br\>
+     * Run the main scene loop.<br\>
+     * Destroy the window.
+     */
     public void showWindow() {
         /*
          * scenes.Main game loop
          */
         glfwShowWindow(glfwWindow);
-
+        
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
+        
         double frameBeginTime = glfwGetTime();
         double frameEndTime;
-
+        
         sceneManager.enable();
-
+        
         while (!glfwWindowShouldClose(glfwWindow)) {
-
+            
             frameEndTime = glfwGetTime();
             Engine.updateDeltaTime((float) (frameEndTime - frameBeginTime));
             frameBeginTime = frameEndTime;
-
+            
             Mouse.update();
             Keyboard.update();
             // poll GLFW for input events
             glfwPollEvents();
-
+            
             sceneManager.update();
             sceneManager.updateGameObjects();
             sceneManager.render();
@@ -172,40 +255,58 @@ public class Window {
             sceneManager.postProcess(currentScene().renderer.fetchColorAttachment(0));
             PostProcessing.finish();
             sceneManager.debugRender();
-
+            
             glfwSwapBuffers(glfwWindow);
             getFPS();
         }
-
+        
         currentScene().clean();
         // Delete all framebuffers
         Framebuffer.clean();
-
+        
         glfwDestroyWindow(glfwWindow);
         glfwTerminate();
         glfwSetErrorCallback(null).free();
     }
-
+    
+    /**
+     * @return The current scene.
+     */
     public Scene currentScene() {
         return sceneManager.currentScene();
     }
-
+    
+    /**
+     * @return The scene manager. - {@link Window#sceneManager}
+     */
     public SceneManager getSceneManager() {
         return sceneManager;
     }
-
+    
+    /**
+     * @param newHeight The new height of the window. Intended for usage in the WindowSizeCallback.
+     */
     private static void setHeight(int newHeight) {
         height = newHeight;
     }
-
+    
+    /**
+     * @param newWidth The new width of the window. Intended for usage in the WindowSizeCallback.
+     */
     private static void setWidth(int newWidth) {
         width = newWidth;
     }
-
+    
+    /**
+     * @return The width of the current window.
+     */
     public static int getWidth() {
         return width;
     }
-
+    
+    /**
+     * @return The height of the current window.
+     */
     public static int getHeight() {
         return height;
     }
--- src/main/java/util/Engine.java
+++ src/main/java/util/Engine.java
@@ -6,73 +6,123 @@ import scene.SceneManager;
 
 import static org.lwjgl.glfw.GLFW.glfwInit;
 
+/**
+ * The Engine class initializes GLFW and the game loop. <br\>
+ * Singleton.
+ */
 public final class Engine {
-
     /**
-     * The Engine class initializes GLFW and the game loop.
+     * The static {@code Engine} instance.
      */
-
     private static final Engine instance = new Engine();
-
+    
+    /**
+     * The exact time the engine started in milliseconds.
+     */
     private final long startMillis;
+    
+    /**
+     * No clue
+     */
     private Window window;
+    
+    /**
+     * Whether the {@code Engine} is running.
+     */
     private boolean running;
+    
+    /**
+     * The time between last frame and current
+     */
     private float deltaTime;
-
+    
     //private to prevent creating new instances
+    /**
+     * Private constructor so that creation of new instances is prohibited.
+     */
     private Engine() {
         running = true;
         deltaTime = 0;
         startMillis = System.currentTimeMillis();
     }
-
+    
     /**
      * Get the global unique instance of the Engine object.
      */
     public static Engine getInstance() {
         return instance;
     }
-
+    
+    /**
+     * @return The time difference between last frame and current. - {@link Engine#deltaTime}
+     */
     public static float deltaTime() {
         return getInstance().getDeltaTime();
     }
-
+    
+    /**
+     * Updates {@link Engine#deltaTime}
+     * @param deltaTime The new value.
+     */
     public static void updateDeltaTime(float deltaTime) {
         getInstance().deltaTime = deltaTime;
     }
-
+    
+    /**
+     * @return Whether the {@code Engine} is running. - {@link Engine#running}
+     */
     public static boolean isRunning() {
         return getInstance().running;
     }
-
+    
+    /**
+     * @return The {@link Window} of the current instance.
+     */
     public static Window window() {
         return getInstance().getWindow();
     }
-
+    
+    /**
+     * @return The {@link Window}.
+     */
     public Window getWindow() {
         return window;
     }
-
+    
+    /**
+     * @return The time difference between last frame and current. - {@link Engine#deltaTime}
+     */
     public float getDeltaTime() {
         return deltaTime;
     }
-
+    
+    /**
+     * Makes the window visible and runs the main window loop.
+     */
     public static void showWindow() {
         window().showWindow();
     }
-
+    
+    /**
+     * @return The {@link SceneManager} of the window.
+     */
     public static SceneManager scenes() {
         return window().getSceneManager();
     }
-
+    
     //internal method called before any init
+    /**
+     * Run before {@link Engine#init}.
+     * <li>Set the GLFW error callback.</li>
+     * <li>Initialize GLFW.</li>
+     */
     private static void preInit() {
         GLFWErrorCallback.createPrint(System.err).set();
-
+        
         if (!glfwInit())
             throw new IllegalStateException("[FATAL] Failed to initialize GLFW.");
     }
-
+    
     /**
      * Start the engine, and initialize GLFW.
      *
@@ -85,7 +135,7 @@ public final class Engine {
         preInit();
         getInstance().window = new Window(windowWidth, windowHeight, windowTitle, minSceneLighting, false);
     }
-
+    
     /**
      * Start the engine, and initialize GLFW.
      *
@@ -97,7 +147,7 @@ public final class Engine {
         preInit();
         getInstance().window = new Window(windowWidth, windowHeight, windowTitle, false);
     }
-
+    
     /**
      * Start the engine, and initialize GLFW. This will create a fullscreen window.
      *
@@ -108,12 +158,12 @@ public final class Engine {
         preInit();
         getInstance().window = new Window(windowTitle, minSceneLighting, false);
     }
-
+    
     public static void init(int windowWidth, int windowHeight, String windowTitle, float minSceneLighting, boolean recalculateProjectionOnResize) {
         preInit();
         getInstance().window = new Window(windowWidth, windowHeight, windowTitle, minSceneLighting, recalculateProjectionOnResize);
     }
-
+    
     /**
      * Start the engine, and initialize GLFW.
      *
@@ -123,12 +173,12 @@ public final class Engine {
         preInit();
         getInstance().window = new Window(windowTitle);
     }
-
+    
     /**
      * @return Returns the number of milliseconds since the engine started. (since the first call)
      */
     public static double millisRunning() {
         return System.currentTimeMillis() - getInstance().startMillis;
     }
-
+    
 }
