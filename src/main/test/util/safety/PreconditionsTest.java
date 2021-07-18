6004b1877fa0710c1262cf1350d08b1d08283804
Juyas <juyasetgames@gmail.com>
2021-07-19 00:34:56 +0200

    + add PreconditionsTest (cause I forgot on branch of this class)

---
+++ src/main/test/util/safety/PreconditionsTest.java
@@ -0,0 +1,36 @@
+package util.safety;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * <h1>Azurite</h1>
+ *
+ * @author Juyas
+ * @version 19.07.2021
+ * @since 19.07.2021
+ */
+public class PreconditionsTest {
+
+    @Test
+    public void nonNull() {
+        String object = "Any String, test";
+        Assert.assertEquals(object, Preconditions.nonNull(object));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void isNull() {
+        Preconditions.nonNull(null);
+    }
+
+    @Test
+    public void isNull2() {
+        String name = "obj";
+        try {
+            Preconditions.nonNull(name, null);
+        } catch (NullPointerException e) {
+            Assert.assertEquals(name + " is null", e.getMessage());
+        }
+    }
+
+}
