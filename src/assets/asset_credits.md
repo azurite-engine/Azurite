b7fe2ac79496ead4e84e1f713ce8fdc72c60c829
Asher <asher.haun@outlook.com>
2022-05-15 18:58:12 -0500

    BASIC button rendering

--- src/assets/asset_credits.md
+++ src/assets/asset_credits.md
@@ -1,3 +1,4 @@
 ### Credits and attributions for media and assets in Azurite
 * Lines of code was created by Trevor Lentz and made available under the Creative Commons Attribution-ShareAlike 3.0 Unported [(CC BY-SA 3.0) license](https://creativecommons.org/licenses/by-sa/3.0/
 ). Downloaded from Trevor Lentz's [OpenGameart page](https://opengameart.org/content/lines-of-code)
+* Button texture - StumpyStrust on [OpenGameArt.org](https://opengameart.org/content/ui-button)
--- src/main/java/scenes/UIRenderingDemo.java
+++ src/main/java/scenes/UIRenderingDemo.java
@@ -6,13 +6,10 @@ import graphics.Window;
 import input.Mouse;
 import org.joml.Vector2f;
 import scene.Scene;
-import ui.Container;
-import ui.ElementRenderer;
-import ui.Layer;
-import ui.Text;
+import ui.*;
 import ui.element.Button;
 import ui.fonts.Font;
-import ui.layout.BoxLayout;
+import ui.layout.AbsoluteLayout;
 import util.Engine;
 
 import static graphics.Graphics.setDefaultBackground;
@@ -22,8 +19,8 @@ public class UIRenderingDemo extends Scene {
     Font openSans;
     Text movingText;
 
-    ElementRenderer e;
-    ElementRenderer f;
+    RenderableElement e;
+    RenderableElement f;
 
     public static void main(String[] args) {
         Engine.init(900, 600, "Azurite UI Rendering Demo");
@@ -40,14 +37,14 @@ public class UIRenderingDemo extends Scene {
         setDefaultBackground(Color.WHITE);
 
         openSans = new Font("src/assets/fonts/OpenSans-Regular.ttf", 20, true);
-        movingText = new Text("HAHA", openSans, Color.GREEN, 200, 200);
+//        movingText = new Text("HAHA", openSans, Color.BLACK, 200, 200);
 
-        menu = new Layer(100, 0, Window.getWidth(), Window.getHeight());
-        Container container = new Container(100, 100, 500, 400, new BoxLayout(BoxLayout.Orientation.VERTICAL));
+        menu = new Layer(0, 0, Window.getWidth(), Window.getHeight());
+        Container container = new Container(100, 100, 500, 400, new AbsoluteLayout());
         menu.registerComponent(container);
 
-        e = new ElementRenderer(Color.RED, new Vector2f(100, 100));
-        f = new ElementRenderer(Color.GREEN, new Vector2f(100, 100));
+        e = new Button("Button", "src/assets/images/button-300-84.png", new Frame(10, 10, 300, 84));
+        f = new RenderableElement(Color.decode("#4e6b56"), new Frame(10, 110, 300, 84));
 
         container.addComponent(e);
         container.addComponent(f);
@@ -60,7 +57,6 @@ public class UIRenderingDemo extends Scene {
 
     public void update () {
         menu.update();
-        movingText.setPosition(Mouse.mouse);
     }
 
 }
